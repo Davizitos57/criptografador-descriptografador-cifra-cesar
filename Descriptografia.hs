133e36b0e533842ed0368f9e3623e8e13ad4f9a1
HalissonPiovezana <halissonpiovezana5@gmail.com>
2026-05-23 16:43:55 -0300

    feat: implementa interface via console e base da função de descriptografia

--- Descriptografia.hs
+++ Descriptografia.hs
@@ -1,1 +1,8 @@
 module Descriptografia where
+
+descriptografar :: String -> Int -> String
+descriptografar msg deslocamento = "..."
+
+-- Deve deslocar para a esquerda
+deslocaChar :: Int -> Char -> Char
+deslocaChar deslocamento caracter = caracter 
--- Main.hs
+++ Main.hs
@@ -1,11 +1,33 @@
-module Main where
+module TP.Main where
 
 import Criptografia
 import Descriptografia
 
 main :: IO ()
 main = do
-    putStrLn "Digite a mensagem:"
-    mensagem <- getLine 
-    putStrLn "Resultado:"
-    putStrLn mensagem
+    putStrLn "=== Cifra de César ==="
+    
+    -- Recebe a entrada como String
+    putStr "Digite a mensagem: "
+    mensagem <- getLine
+    
+    putStr "Digite o valor de deslocamento: "
+    -- Lê a linha e converte para Int
+    deslocamento <- readLn :: IO Int
+    
+    putStrLn "\nEscolha uma opção:"
+    putStrLn "1 - Criptografar"
+    putStrLn "2 - Descriptografar"
+    putStr "Opção: "
+    opcao <- getLine
+    
+    case opcao of
+        "1" -> do
+            let resultado = criptografar mensagem deslocamento
+            putStrLn $ "\nMensagem Criptografada: " ++ resultado
+            
+        "2" -> do
+            let resultado = descriptografar mensagem deslocamento
+            putStrLn $ "\nMensagem Descriptografada: " ++ resultado
+            
+        _ -> putStrLn "\nOpção inválida. Tente novamente."
