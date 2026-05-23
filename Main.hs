78502f47ee776affec6daf4689d92fa9d5392ce0
Davi Abner <davi.abner57@gmail.com>
2026-05-23 15:49:04 -0300

    feat(main): cria função básica para leitura e saída de mensagem

--- Main.hs
+++ Main.hs
@@ -3,4 +3,9 @@ module Main where
 import Criptografia
 import Descriptografia
 
-main :: IO()
+main :: IO ()
+main = do
+    putStrLn "Digite a mensagem:"
+    mensagem <- getLine 
+    putStrLn "Resultado:"
+    putStrLn mensagem
