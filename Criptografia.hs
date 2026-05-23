abeca66f70595c8be3a80c93b93eac2b95dc73ae
Davi Abner <davi.abner57@gmail.com>
2026-05-23 16:47:19 -0300

    feat(Criptografia): cria a função de criptografar caracteres baseado no valor passado em cifra de cesar

--- Criptografia.hs
+++ Criptografia.hs
@@ -1,2 +1,8 @@
 module Criptografia where    
 
+deslocaChar :: Int -> Char -> Char
+deslocaChar i c
+    | c >= 'A' && c <= 'Z' = toEnum (((fromEnum c - fromEnum 'A' + i) `mod` 26) + fromEnum 'A') 
+    | c >= 'a' && c <= 'z' = toEnum (((fromEnum c - fromEnum 'a' + i) `mod` 26) + fromEnum 'a') 
+    | otherwise = c
+    
