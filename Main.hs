2f72831617de4af68fde12d32db265a60ec6bc54
Davi Abner <davi.abner57@gmail.com>
2026-05-24 12:22:31 -0300

    feat(Main): implementa loop para garantir que o sistema não feche após uso de alguma funcionalidade

--- Main.hs
+++ Main.hs
@@ -5,29 +5,38 @@ import Descriptografia
 
 main :: IO ()
 main = do
-    putStrLn "=== Cifra de César ==="
-    
-    -- Recebe a entrada como String
+    putStrLn "\n=== Cifra de César ==="
+    menu
+
+menu :: IO ()
+menu = do 
     putStr "Digite a mensagem: "
     mensagem <- getLine
     
     putStr "Digite o valor de deslocamento: "
-    -- Lê a linha e converte para Int
     deslocamento <- readLn :: IO Int
     
     putStrLn "\nEscolha uma opção:"
     putStrLn "1 - Criptografar"
     putStrLn "2 - Descriptografar"
-    putStr "Opção: "
+    putStrLn "Para sair do sistema basta digitar o número 0"
+    putStr "\nOpção: "
     opcao <- getLine
     
     case opcao of
+        "0" -> do
+            putStrLn "Saindo do sistema de Criptografia e Descriptografia..."
+
         "1" -> do
             let resultado = criptografar deslocamento mensagem
             putStrLn $ "\nMensagem Criptografada: " ++ resultado
+            main
             
         "2" -> do
             let resultado = descriptografar deslocamento mensagem
             putStrLn $ "\nMensagem Descriptografada: " ++ resultado
+            main
             
-        _ -> putStrLn "\nOpção inválida. Tente novamente."
+        _ -> do
+            putStrLn "\nOpção inválida. Tente novamente."
+            main
